0bd168a74f71e25e3ac180fe329e2857fc21ae35
mrshabel <sgumah001@st.ug.edu.gh>
2023-12-13 07:05:42 +0000

    add blog content source

---
+++ db/blogs.js
@@ -0,0 +1,21 @@
+export const blogs = [
+  {
+    title: "KDB: Impossible is Nothing",
+    slug: "kdb-impossible-is-nothing",
+    summary:
+      "The Kevin De Bruyne story — a Chelsea flop who etched his name unto the premier league hall of fame.",
+    date: "2023-12-13",
+    content: `
+      Coming up as a young forward in Jose Mourinho’s Chelsea, Kevin De Bruyne, together with Mohamed Salah got labeled as protagonists in the stacked Chelsea team. KDB, however, due to the inconsistencies in playing time had to move abroad to a mid-table team, Wolfsburg in hopes of securing a starting eleven spot.
+
+      Little did the blonde Belgian folk know that the grass was greener on the other side. Throughout his stay in the Bundesliga, the rejected Chelsea man put up a remarkable display of 21 assists in the 14/15 league season. This feat ultimately led to him winning the Bundesliga Player of the Season.
+
+      A shocking return to the EPL by KDB was announced in the Summer of 2015 by Manchester City which led to the talks of KDB being on a “fraud-watch” by the English Media.
+
+      Within his 8-year stay in England, KDB has broken numerous records while leading his team to silverware. Notable mentions include the club’s first UCL, Quadruple, Treble, and Centurions. Not to forget, KDB has won the PFA Premier League Player of the Season in back-to-back seasons being the third player to do so in English Football history.
+
+      It gets worse at times when football fans are blinded by nostalgia but by the time he hangs up his boots, I hope everyone comes in unison with the sole assertion that Kevin de Bruyne is the greatest midfielder to ever walk the English Premier League.
+    `,
+    image: "/images/kdb_blog_image.jpg",
+  },
+];
